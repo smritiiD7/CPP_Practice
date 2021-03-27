c06009f0acbf396311468387b8dda9c233cac764
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2021-03-27 13:06:24 +0530

    Time: 4 ms (98.53%), Space: 10.7 MB (49.49%) - LeetHub

---
+++ slowest-key/slowest-key.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    char slowestKey(vector<int>& releaseTimes, string s) {
+            vector<char> ch;
+            vector<int> rem;
+            int i;
+            char lexo=s[0],ans;
+            rem.push_back(releaseTimes[0]);
+            ch.push_back(s[0]);
+           // ch[0]=s[0];
+            int TimeTaken,max=releaseTimes[0];
+            //char alpha;
+            for(i=1;i<releaseTimes.size();i++)
+            {
+                 TimeTaken = releaseTimes[i] - releaseTimes[i - 1];
+                    if(TimeTaken>max){
+                            max=TimeTaken;
+                            ch.push_back(s[i]);
+                            lexo=s[i];
+                    }
+                    if(TimeTaken==max)
+                    {
+                            max=TimeTaken;
+                            if(s[i]>s[i-1])
+                            {
+                                    lexo=s[i];
+                            }
+                            
+                    }
+            }
+            ans=lexo;
+            return ans;
+           
+        
+    }
+};
