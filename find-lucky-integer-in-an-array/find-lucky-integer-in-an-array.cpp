b5fa1f61676157485a4cb94b6e4c0d73e96af42a
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2021-03-30 20:35:52 +0530

    Time: 0 ms (100.00%), Space: 10 MB (92.55%) - LeetHub

---
+++ find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+            int i,maxElement=0,maxLucky=-1,lucky,hash[501]={0};
+            vector<int> v;
+           int  maxIn=*max_element(arr.begin(),arr.end());
+            for(i=0;i<arr.size();i++)
+            {
+                    hash[arr[i]]++;
+            }
+            for(i=1;i<=maxIn;i++)
+            {
+                    int k=i;
+                    if(k==hash[i])
+                    {
+                            
+                        if(k>=maxLucky)
+                        {
+                                maxLucky=k;
+                        }
+                    }
+             }
+           
+            return maxLucky;
+          
+    }
+};
