aca2f78136dd31ef4ef943d671cf4a7149206417
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2021-04-04 10:24:45 +0530

    Time: 0 ms (100.00%), Space: 6 MB (98.52%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(string s) {
+            int i,l=s.length();
+            vector<char> v;
+            if(l==1)
+                    return false;
+          
+            for(i=0;i<l;i++)
+            {
+                    if(s[i]=='(')
+                    {
+                            v.push_back(')');
+                    }
+                   else if(s[i]=='{')
+                    {
+                            v.push_back('}');
+                    }
+                   else if(s[i]=='[')
+                    {
+                            v.push_back(']');
+                    }
+                   else
+                   {
+                           if((v.empty())||(s[i]!=v[v.size()-1]))
+                            return false;
+                            v.pop_back();
+                    
+                   }
+            }
+           
+            return v.empty();
+        
+    }
+};
