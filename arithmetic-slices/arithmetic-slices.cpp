12090222cf5a908773ec2567d19d1b81078d2a34
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2021-05-19 12:43:14 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (99.04%) - LeetHub

---
+++ arithmetic-slices/arithmetic-slices.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int i,cnt=0,res;
+        for(i=2;i<nums.size();i++)
+        {
+            if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2])
+            {
+                cnt++;
+            }
+            else{
+               
+                cnt=0;
+            }
+                 res+=cnt;
+            
+        }
+        return res;
+        
+    }
+};
