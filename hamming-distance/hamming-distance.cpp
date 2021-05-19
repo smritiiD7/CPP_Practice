a4666902c475c2cf4526cb35cb490ffbdf798df3
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2021-05-19 17:46:24 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (94.27%) - LeetHub

--- hamming-distance/hamming-distance.cpp
+++ hamming-distance/hamming-distance.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        int ax[34]={0},ay[34]={0},i=0,j=0,cnt=0,k,l;
-        while(x>0)
+        int n=x^y;
+        int cnt=0;
+        while(n)
         {
-           ax[i]=x%2;
-            x=x/2;
-            i++;
-        }
-        while(y>0)
-        {
-            ay[j]=y%2;
-            y=y/2;
-            j++;
-        }
-        for(k=0,l=0;k<34,l<34;k++,l++)
-        {
-            if(ax[k]!=ay[l])
-            {
-                cnt++;
-            }
+            n=n&(n-1);
+            cnt++;
         }
         return cnt;
-        
     }
 };
