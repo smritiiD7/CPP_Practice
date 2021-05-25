c99d971a943058612ee7823307ba9dff5cf9404f
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2021-05-25 12:48:29 +0530

    Time: 8 ms (89.94%), Space: 10.9 MB (57.39%) - LeetHub

--- longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.cpp
+++ longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findLengthOfLCIS(vector<int>& nums) {
-        int i,cnt=1,maxi=INT_MIN;
+        int i,cnt=1,maxi=1;
         if(nums.size()==1)
             return 1;
         for(i=1;i<nums.size();i++)
@@ -9,10 +9,11 @@ public:
             if(nums[i-1]<nums[i])
             {
                 cnt++;
+                 maxi=max(cnt,maxi);
             }
             else
                 cnt=1;
-            maxi=max(cnt,maxi);
+           
         }
         return maxi;
     }
