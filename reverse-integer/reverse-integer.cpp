31848fbb59145acc3e12b9ed7d5e0f930b644888
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2021-05-29 10:56:24 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (71.43%) - LeetHub

---
+++ reverse-integer/reverse-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+       long long  ans=0;
+        while(x)
+        {
+            ans=ans*10+x%10;
+            x=x/10;
+        }
+        return (ans>INT_MAX||ans<INT_MIN)?0:ans;
+        
+    }
+};
