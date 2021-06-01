4632f2597dc9191ad362ae74ea3b8e087f91d2b3
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2021-06-01 13:11:42 +0530

    Time: 12 ms (96.34%), Space: 16.9 MB (46.50%) - LeetHub

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int i=0;
-        for(int j: nums)
+        int i,x=0;
+        for(i=0;i<nums.size();i++)
         {
-            i^=j;
+             x^=nums[i];
         }
-        return i;
+        return x;
         
     }
 };
