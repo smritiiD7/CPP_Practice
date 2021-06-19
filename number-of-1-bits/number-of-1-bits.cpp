c6a6201d1d58dd12b0912659f701c1273686a7be
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2021-06-19 23:08:26 +0530

    Time: 4 ms (48.68%), Space: 5.9 MB (42.54%) - LeetHub

--- number-of-1-bits/number-of-1-bits.cpp
+++ number-of-1-bits/number-of-1-bits.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int cnt=0;
-        while(n)
+        while(n!=0)
         {
-            n&=n-1;
+            n=n&(n-1);
             cnt++;
         }
         return cnt;
