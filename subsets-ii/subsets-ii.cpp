4ba94d51585979d7dec06b770e4e80cd648d8cfa
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2021-06-23 09:30:54 +0530

    Time: 8 ms (32.97%), Space: 8.5 MB (32.94%) - LeetHub

---
+++ subsets-ii/subsets-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        set<vector<int>> s;
+        int i,j;
+        int subsets=pow(2,nums.size());
+        for(i=0;i<subsets;i++)
+        {
+            vector<int> temp;
+            for(j=0;j<nums.size();j++)
+            {
+                if((i&(1<<j)))
+                   {
+                      temp.push_back(nums[j]); 
+                   }
+            }
+           sort(temp.begin(),temp.end());
+           s.insert(temp);
+        }
+        for(auto &i: s)
+        {
+            ans.push_back(i);
+        }
+        return ans;
+        
+    }
+};
