a2f64ede7aaf4261393b15b94629a933e081f248
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2021-07-12 10:04:30 +0530

    Time: 12 ms (54.89%), Space: 7.3 MB (37.68%) - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,21 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int hashs[26]={0},hasht[26]={0},i;
-        if(s.length()!=t.length())
-            return false;
-        for(i=0;i<s.length();i++)
-        {
-            hashs[s[i]-'a']++;
-            hasht[t[i]-'a']++;
-        }
-        for(i=0;i<26;i++)
-        {
-            if(hashs[i]!=hasht[i])
-                return false;
-        }
-        return true;
-        
-        
+       sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
