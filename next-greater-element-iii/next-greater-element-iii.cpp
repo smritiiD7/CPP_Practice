1b53d701bcb7a8c596568cbe8f74989cbcd4787f
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2021-08-25 09:47:40 +0530

    Time: 0 ms (100.00%), Space: 6 MB (27.68%) - LeetHub

---
+++ next-greater-element-iii/next-greater-element-iii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string s=to_string(n);
+        int i=s.length()-2;
+        for(;i>=0&&s[i]>=s[i+1];i--){}
+        if(i==-1) return -1;
+        reverse(s.begin()+i+1,s.end());
+        for(int j=i+1;j<s.length();j++)
+        {
+            if(s[j]>s[i])
+            {
+                swap(s[i],s[j]);
+                break;
+            }
+        }
+        long ans=stol(s);
+        return ans==n||ans>INT_MAX?-1:ans;
+    }
+};
