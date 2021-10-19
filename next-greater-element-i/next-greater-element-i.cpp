64cef134d52fcfbefe432b5b1f4c697112cc7d1e
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2021-10-19 09:14:14 +0530

    Time: 7 ms (63.29%), Space: 9 MB (31.90%) - LeetHub

--- next-greater-element-i/next-greater-element-i.cpp
+++ next-greater-element-i/next-greater-element-i.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-       unordered_map<int,int> mp;
-        stack<int> s;
-        vector<int> v;
-        for(int i=nums2.size()-1;i>=0;i--)
+        unordered_map<int,int> mp;
+        stack<int> st;
+        vector<int> ans;
+        for(int i = nums2.size()-1;i>=0;i--)
         {
-            if(s.size()==0)
-            {
+            if(st.size()==0)
                 mp[nums2[i]]=-1;
-            }
-            else if(s.size()>0&&s.top()>nums2[i])
+            else if(st.size()>0 && st.top()>nums2[i])
+                mp[nums2[i]]=st.top();
+            else if(st.size()>0 && st.top()<=nums2[i])
             {
-                mp[nums2[i]]=s.top();
-            }
-            else if(s.size()>0 && s.top()<=nums2[i])
-            {
-                while(s.size()>0 && s.top()<=nums2[i])
+                while(st.size()>0 && st.top()<=nums2[i])
                 {
-                    s.pop();
-                }
-                if(s.size()==0)
-                {
-                    mp[nums2[i]]=-1;
-                }
-                else
-                {
-                    mp[nums2[i]]=s.top();
+                    st.pop();
+                    if(st.size() == 0)
+                        mp[nums2[i]]=-1;
+                    else
+                        mp[nums2[i]]=st.top();
                 }
             }
-            s.push(nums2[i]);
+            st.push(nums2[i]);
         }
-        for(int k: nums1)
-        {
-            v.push_back(mp[k]);
-        }
-        return v;
+         for(int i=0;i<nums1.size();i++)
+         {
+             ans.push_back(mp[nums1[i]]);
+         }
+        return ans;
     }
 };
