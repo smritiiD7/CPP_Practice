f29c5d5afa90d054920679b6682ef88af955e78c
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2022-02-18 00:52:43 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (82.60%) - LeetHub

---
+++ 397-integer-replacement/397-integer-replacement.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int integerReplacement(int k) {
+        long long n=k;
+        int cnt=0;
+        while(n>1)
+        {
+            if(n%2==0)
+            {
+                n=n/2;
+                cnt++;
+            }
+            else
+            {
+                if(n==3 || n==5)
+                    n=n-1;
+                
+                else{
+                if(((n+1)/2)%2==0)
+                    n=n+1;
+                    else
+                        n=n-1;
+                
+                }
+                cnt++;;
+            }
+        }
+        return cnt;
+    }
+};
