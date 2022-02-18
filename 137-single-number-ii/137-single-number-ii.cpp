a6a1e424d5e53e660d4e6446cf3d4aa3eab93c36
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2022-02-18 14:17:47 +0530

    Time: 3 ms (99.05%), Space: 10.1 MB (13.83%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int i: nums)
+        {
+            mp[i]++;
+        }
+        for(auto i : mp)
+        {
+            if(i.second==1)
+                return i.first;
+        }
+        return -1;
+    }
+};
