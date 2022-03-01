322e087a31a5fa97727404c12498f4d1ebf7d664
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2022-03-01 10:29:34 +0530

    Time: 13 ms (20.03%), Space: 7.7 MB (89.06%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res(n+1,0);
+        if(n==0) return res;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==0)
+                res[i]=res[i/2];
+            else
+                res[i]=res[i-1]+1;
+        }
+        return res;
+    }
+};
