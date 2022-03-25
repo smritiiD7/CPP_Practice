cec45f947d0b1a1b633b9f22ea145f9a50446f11
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2022-03-25 09:50:01 +0530

    Time: 3 ms (90.96%), Space: 7.9 MB (78.03%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        priority_queue<pair<int,int>> pq;
-        int res=0;
-        for(int i=0;i<costs.size();i++)
+       priority_queue<pair<int,int>> pq;
+        int ans=0;
+       for(int i=0;i<costs.size();i++)
         {
             pq.push(make_pair(costs[i][0] - costs[i][1],i));
                     
         }
-       // int halfsize = costs.size/2;
-        for(int i=0;i<costs.size()/2;i++)
+        for(int i = 0 ;i < costs.size()/2;i++)
         {
-            res+=costs[pq.top().second][1];
+            ans+=costs[pq.top().second][1];
             pq.pop();
         }
-         for(int i=0;i<costs.size()/2;i++)
+        for(int i = 0 ;i < costs.size()/2;i++)
         {
-            res+=costs[pq.top().second][0];
+            ans+=costs[pq.top().second][0];
             pq.pop();
         }
-        return res;
+        
+        
+        
+        
+        
+        return ans;
+        
     }
 };
