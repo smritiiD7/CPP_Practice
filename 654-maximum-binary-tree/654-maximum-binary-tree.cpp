67c15294546d3b531d9a25118215941d60a7ac27
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2022-04-29 11:13:36 +0530

    Time: 106 ms (60.47%), Space: 42.6 MB (34.59%) - LeetHub

---
+++ 654-maximum-binary-tree/654-maximum-binary-tree.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+     
+    int findMax(int start, int end, vector<int>& nums)
+    {
+        int index=start;
+        for(int i=start;i<=end;i++)
+        {
+            if(nums[i]>nums[index])
+            {
+
+                index=i;
+            }
+        }
+        return index;
+    }
+    
+    
+    TreeNode* buildTree(int start,int end, vector<int>& nums)
+    {
+        if(start>end) return NULL;
+        if(start==end) return new TreeNode(nums[start]);
+        int index=findMax(start,end,nums);
+        TreeNode *node = new TreeNode(nums[index]);
+        node->left=buildTree(start,index-1,nums);
+        node->right=buildTree(index+1,end,nums);
+        return node;
+    }
+   
+    
+    
+    
+    
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+      
+        int n = nums.size();
+        return buildTree(0,n-1,nums);
+        
+    }
+};
