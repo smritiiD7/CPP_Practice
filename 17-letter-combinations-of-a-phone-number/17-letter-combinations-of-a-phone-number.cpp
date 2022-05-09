390dc8e4a6358ec495413ce4bfe9c023ba99650e
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2022-05-09 13:32:44 +0530

    Time: 0 ms (100.00%), Space: 7 MB (9.47%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    unordered_map<char,vector<string>> mp;
+    vector<string> ans;
+    void backtrack(int firstIndex,string curr, string digits)
+    {
+        int n = digits.size();
+        if(firstIndex==n)
+        {
+            ans.push_back(curr);
+        }
+        vector<string> v = mp[digits[firstIndex]];
+        for(int i=0;i<v.size();i++)
+        {
+            string s = v[i];
+            curr+=s;
+            backtrack(firstIndex+1,curr,digits);
+            curr.pop_back();
+        }
+    }
+   
+       
+    
+    vector<string> letterCombinations(string digits) {
+        if(digits=="")
+        {
+            return {};
+        }
+        
+    mp['2'] = {"a","b","c"};
+	mp['3'] = {"d","e","f"};
+	mp['4'] = {"g","h","i"};
+	mp['5'] = {"j","k","l"};
+	mp['6'] = {"m","n","o"};
+	mp['7'] = {"p","q","r","s"};
+	mp['8'] = {"t","u","v"};
+	mp['9'] = {"w","x","y","z"};
+        backtrack(0,"",digits);
+        return ans;
+      
+    }
+};
