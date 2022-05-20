e1df4c85a584be487886baba7bb3b82102807ad3
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2022-05-20 14:18:12 +0530

    Time: 11 ms (16.35%), Space: 7.7 MB (65.13%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int time=0;
+      while(tickets[k]>0)
+      {
+          for(int i=0;i<tickets.size()&& tickets[k]>0;i++)
+          {
+              if(tickets[i]>0){
+              tickets[i]--;
+              time++;
+              }
+             
+          }
+       
+    }
+         return time;
+    }
+};
