42da59f064fd06b0509655ebe46ca471e97efbc0
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2022-06-18 10:49:44 +0530

    Time: 3 ms (78.22%), Space: 8.5 MB (73.05%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-      int cummulativeSum=nums[0];
-      for(int i=1;i<nums.size();i++)
-      {
-          cummulativeSum+=nums[i];
-          nums[i]=cummulativeSum;   
-      }
+        int cummulativeSum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            cummulativeSum+=nums[i];
+            nums[i]=cummulativeSum;
+        }
         return nums;
     }
 };
