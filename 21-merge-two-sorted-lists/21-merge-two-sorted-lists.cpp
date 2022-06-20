460f49affa4b34df55acc915fa9f8e558eb547f6
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2022-06-20 14:11:50 +0530

    Time: 7 ms (85.46%), Space: 15.1 MB (8.21%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -10,30 +10,36 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-           map<int,int> mp;
-        ListNode *curr1(l1),*curr2(l2);
-        while(curr1)
-        {
-            mp[curr1->val]++;
-            curr1=curr1->next;
-        }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL) return list2;
+        if(list2==NULL) return list1;
+       vector<int> temp;
+       ListNode *curr1(list1);
+       ListNode *curr2(list2);
+    
+       while(curr1)
+       {
+           temp.push_back(curr1->val);
+           curr1=curr1->next;
+       }
+       
         while(curr2)
         {
-            mp[curr2->val]++;
+            temp.push_back(curr2->val);
             curr2=curr2->next;
         }
-        ListNode *dummy=new ListNode(0);
-        ListNode *curr(dummy);
-        for(auto i: mp)
+        
+        sort(temp.begin(),temp.end());
+        
+        ListNode *list3 = new ListNode(temp[0]);
+        ListNode *head(list3);
+        for(int i=1;i<temp.size();i++)
         {
-            while(i.second>0)
-            {
-                curr->next=new ListNode(i.first);
-                curr=curr->next;
-                i.second--;
-            }
+            ListNode *tempNode = new ListNode(temp[i]);
+            list3->next = tempNode;
+            list3=list3->next;
         }
-        return dummy->next;
+        
+        return head;
     }
 };
