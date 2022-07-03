33e94a9f47ad49de824a1d442ae3706867a4dc03
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2022-07-03 11:02:28 +0530

    Time: 15 ms (13.93%), Space: 10.5 MB (11.54%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -10,28 +10,33 @@
  */
 class Solution {
 public:
-      ListNode *partition(ListNode* head, int x) {
-      ListNode *firstHalf = new ListNode(-1);
-      ListNode *secondHalf = new ListNode(-1);
-      ListNode *res(firstHalf);
-      ListNode *secondStart(secondHalf);
-      ListNode *curr(head);
-        while(curr)
-        {
-            if(curr->val<x)
-            {
-                firstHalf->next =new ListNode(curr->val);
-                firstHalf=firstHalf->next;
-                curr=curr->next;
-            }
-            else{
-                secondHalf->next = new ListNode(curr->val);
-                secondHalf=secondHalf->next;
-                curr=curr->next;
-
-            }
-        }
-        firstHalf->next=secondStart->next;
-        return res->next;
+    ListNode* partition(ListNode* head, int x) {
+      ListNode* firstHalf = new ListNode(-1);
+      ListNode* newhead(firstHalf);
+      ListNode* secondHalf = new ListNode(-1);
+      ListNode* secondHalfStart(secondHalf);
+        
+     while(head)
+     {
+         if(head->val < x)
+         {
+             firstHalf->next = new ListNode(head->val);
+             firstHalf= firstHalf->next;
+             head=head->next;
+         }
+         else
+         {
+             secondHalf->next =  new ListNode(head->val);
+             secondHalf = secondHalf->next;
+             head=head->next;
+         }  
+         
+         firstHalf->next = secondHalfStart->next;
+         
+     }
+        return newhead->next;
     }
+        
+        
+    
 };
