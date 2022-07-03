da322f4c89419049563e843d91271eb42971b421
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2022-07-03 10:38:11 +0530

    Time: 4 ms (52.52%), Space: 9.1 MB (29.01%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int k  = n+m-1;
-        int i = m-1,j=n-1;
-        
-        while(i>=0 && j>=0)
+       int fai= m-1;
+       int sai= n-1;
+       int insertAt=m+n-1;
+       
+        while(fai >=0 && sai >=0)
         {
-            if(nums1[i]>nums2[j])
+            if(nums1[fai] >= nums2[sai])
             {
-                nums1[k]=nums1[i];
-                i--;
-                k--;
+                nums1[insertAt]= nums1[fai];
+                insertAt--;
+                fai--;
             }
             else
             {
-                nums1[k]=nums2[j];
-                j--;
-                k--;
+                nums1[insertAt]= nums2[sai];
+                insertAt--;
+                sai--;
             }
         }
-        while(i>=0)
-            nums1[k--] = nums1[i--];
-        while(j>=0)
-            nums1[k--] = nums2[j--];
+        while(fai>=0)
+        {
+            nums1[insertAt--] = nums1[fai--];
+        }
         
+        while(sai>=0)
+        {
+            nums1[insertAt--] = nums2[sai--];
+        }
     }
 };
