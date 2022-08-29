0fadb44d017bd2781a9d7931aa770967aaad4b7c
Smriti Dubey <72901365+smritiiD7@users.noreply.github.com>
2022-08-29 12:22:03 +0530

    Time: 4 ms (32.58%), Space: 6.1 MB (96.10%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
     bool isValid(string s) {
-         int i,l=s.length();
-            vector<char> v;
-            if(l==1)
-                    return false;
-          
-            for(i=0;i<l;i++)
-            {
-                    if(s[i]=='(')
-                    {
-                            v.push_back(')');
-                    }
-                   else if(s[i]=='{')
-                    {
-                            v.push_back('}');
-                    }
-                   else if(s[i]=='[')
-                    {
-                            v.push_back(']');
-                    }
-                   else
-                   {
-                           if((v.empty())||(s[i]!=v[v.size()-1]))
-                            return false;
-                            v.pop_back();
-                    
-                   }
-            }
-           
-            return v.empty();
-        
+      vector<char> v;
+     for(int i=0;i<s.length();i++)
+     {
+         if(s[i]=='(')
+             v.push_back(')');
+        else if(s[i]=='[')
+            v.push_back(']');
+        else if(s[i]=='{')
+            v.push_back('}');
+        else
+        {
+            if(v.empty()==true || s[i]!=v.back())return false;
+            v.pop_back();
+        }
+     }
+        return v.empty();
     }
 };
